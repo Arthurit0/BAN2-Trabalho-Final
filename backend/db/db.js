71f78c8f23de1f74ac4baa0968fecc962f2c8a41
Arthur José <arthur.jose27@gmail.com>
2023-10-23 14:05:50 -0300

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,19 +1,13 @@
 import { Pool } from 'pg';
 
-async function connect() {
-    const pool = new Pool({
-        host: 'localhost',
-        port: 5432,
-        database: 'ban2_trab_final',
-        user: 'postgres',
-        password: 'postgres'
-    });
-
-    const client = await pool.connect();
-    console.log('Pool conectada!');
-
-    global.connection = pool;
-    return pool.connect();
+class ConexaoBD {
+    constructor() {
+        this.pool = new Pool({
+            host: 'localhost',
+            port: 5432,
+            user: 'postgres',
+            password: 'postgres',
+            database: 'ban2_trab_final'
+        })
+    }
 }
-
-export default connect;
